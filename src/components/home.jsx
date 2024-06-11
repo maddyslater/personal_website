53258a4a4c432119a884c6b5c6577259f2d5b345
maddyslater <madeline.g.slater.26@dartmouth.edu>
2024-06-11 03:25:07 -0600

    updated home page

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import '../stylesheet.css';
-import homePic from '../assets/homePic.jpeg'
+import homePic from '../assets/snow.jpg'
 
 const Home = () => {
     return (
         <div className="home-page">
-            <div className="home-text">
+            <div className="home-main-container">
                 <h1 className="home-title">Maddy Slater</h1>
-                <h2 className="home-subtitle">Welcome to my website!</h2>
+                {/* <h2 className="home-subtitle">Welcome to my website!</h2> */}
+                <img id='home-image' src={homePic} alt="Picture of sunset over mountain forest" />
+                
             </div>
-            <img id='home-image' src={homePic} alt="Picture of sunset over mountain forest" />
+            <h2 className="home-link">Read more <a href="#about" >about me ↓</a></h2>
         </div>
     )
 }
